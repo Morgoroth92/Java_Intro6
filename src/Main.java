5aced0b1bbcfc690906a86229d9e152e99e2f80b
Claudio V. Carta <claudio.vittorio.carta@gmail.com>
2023-12-19 14:37:54 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String s = "Ciao prova prova";
+        System.out.println(lunghezzaStringa(s));
+
+    }
+    public static int lunghezzaStringa(String s) {
+        return s.length();
+    }
+}
